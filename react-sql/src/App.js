e5a3de6a3d356f5a4662d2606ece08f428dcde9c
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-02-22 21:18:53 +0530

    done the project

--- react-sql/src/App.js
+++ react-sql/src/App.js
@@ -6,19 +6,30 @@ import { response } from 'express';
 class App extends Component {
 
     state = {
-      products: []
+      products: [],
+      product: {
+        name: 'sample product',
+        price: 20
+      }
     }
 
     componentDidMount() {
-      
+      this.getProducts();
     }
 
     getProducts = _=> {
       fetch('http://localhost:4000/products')
       .then(response => response.json())
-      .then({ data }) => {
-        console.log(data )
+      .then(response => this.setState({ product: response.data })) 
       .catch(err => console.error(err))
+    }
+
+    addProduct = _=> {
+      const { product } = this.state;
+      fetch('http://localhost:4000/products/add?name=${product.name}&price=${product.price}')
+        
+        .then(this.getProducts)
+        .catch(err => console.error(err))
     }
 
   renderProduct = ({ product_id, name }) => <div key={product_id}>{name}</div>
@@ -28,6 +39,15 @@ class App extends Component {
     return (
     <div className="App">
       {products.map(this.renderProduct)}
+
+      <div>
+        <input value={product.name} 
+        onChange={e => this.setState({ product: { ...product, name: e.target.value} })} />
+        <input value={product.price} 
+        onChange={e => this.setState({ product: { ...product, price: e.target.value} })}
+        />
+        <button onClick={this.addProduct}>Add product </button>
+      </div>
     </div>
     );
   }
